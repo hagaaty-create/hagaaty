f494e144b19db7a15920bf9abae5fd8708b8415a
Hagaaty Hagaaty <hagaaty@gmail.com>
2025-11-14 22:27:37 +0000

    Try fixing this error: `Console Error: FirebaseError: Firebase: Error (a

--- src/firebase/client-provider.tsx
+++ src/firebase/client-provider.tsx
@@ -5,16 +5,7 @@ import { initializeApp, getApp, getApps } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { FirebaseProvider } from './provider';
-
-// This is the correct place to build the config object on the client
-export const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-};
+import { firebaseConfig } from './config';
 
 
 export function FirebaseClientProvider({
