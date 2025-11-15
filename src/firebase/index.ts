127e11e885ee46698fe6510ceef7dd3b3943bf5d
Hagaaty Hagaaty <hagaaty@gmail.com>
2025-11-15 18:18:28 +0000

    Skip to content
    Find…
    F
    
    New Project
    importing from GitHub
    
    hagaaty-cre

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -44,8 +44,6 @@ export * from './provider';
 export * from './client-provider';
 export * from './firestore/use-collection';
 export * from './firestore/use-doc';
-// No longer export from here to avoid circular dependency
-// export * from './auth/use-user';
 export * from './non-blocking-updates';
 export * from './non-blocking-login';
 export * from './errors';
